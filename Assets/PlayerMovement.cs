054793219a3698698f2c99768f0771dcf85c96b0
HectorBjernersjo <hector@bjernersjo.se>
2020-12-12 22:16:39 +0100

    Jump funkar

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,30 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CoolCharacterController controller;
+   public CoolCharacterController controller;
+   public GameObject groundCheck;
+   public float runSpeed = 40F;
+   private bool canJump = false;
+   private float jumpForce = 800f;
 
-    public float runSpeed = 40F;
+   float horizontalMove = 0F;
+   bool jump = false;
 
-    float horizontalMove = 0F;
-    bool jump = false;
-
-    // Update is called once per frame
-    void Update()
+   // Update is called once per frame
+   void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
+        if(canJump && Input.GetButtonDown("Jump"))
+           Jump();
     }
 
+    private void Jump()
+    {
+       GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+       canJump = false;
+    }
 
-    void FixedUpdate ()
+   void FixedUpdate ()
     {
 
         //Move our character
@@ -32,4 +38,12 @@ public class PlayerMovement : MonoBehaviour
         jump = false; 
 
     }
+
+   void OnCollisionEnter2D(Collision2D col)
+   {
+      if (col.gameObject.GetComponent<TilemapCollider2D>() != null)
+      {
+         canJump = true;
+      }
+   }
 }
