e61d80431c2777af5b28ca80bb394fb3b3a291ed
IronicDoll <emma.alagic@outlook.com>
2020-12-12 21:07:51 +0100

    Den kan röra sig!! :D

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CoolCharacterController controller;
+
+    public float runSpeed = 40F;
+
+    float horizontalMove = 0F;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+    }
+
+
+    void FixedUpdate ()
+    {
+
+        //Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
