0ababbb75715f6eeb11d02463d0d97f99e9e9ebf
HectorBjernersjo <hector@bjernersjo.se>
2020-12-12 21:09:29 +0100

    Lade till en enemy

---
+++ Assets/Using/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+   int direction = 1;
+   int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       Vector3 movement = new Vector3(direction, 0, 0f);
+       transform.position += movement.normalized * Time.fixedDeltaTime * speed;
+   }
+}
